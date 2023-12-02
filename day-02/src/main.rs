56fea398a406d0dfde79c8c15dac2745e127151d
azmonreal <azmonreal@gmail.com>
2023-12-02 09:54:38 -0600

    day 2: part 2

--- day-02/src/main.rs
+++ day-02/src/main.rs
@@ -1,6 +1,6 @@
+use std::collections::HashMap;
 use std::env;
 use std::fs;
-use std::collections::HashMap;
 
 fn main() {
     let file_path = env::args().nth(1).expect("Please provide a file path");
@@ -8,20 +8,19 @@ fn main() {
 
     let contents = fs::read_to_string(file_path).expect("Something went wrong reading the file");
 
-    println!("Part 1: {}", part1(&contents));
-    // println!("Part 2: {}", part2(&contents));
+    let results = part1(&contents);
+
+    println!("Part 1: {}", results.0);
+    println!("Part 2: {}", results.1);
 }
 
-fn part1(input: &str) -> i32 {
-    let available = HashMap::from([
-        ("red",12),
-        ("green",13),
-        ("blue",14),
-    ]);
+fn part1(input: &str) -> (i32, i32) {
+    let available = HashMap::from([("red", 12), ("green", 13), ("blue", 14)]);
 
-    let mut id_sum : i32 = 0;
+    let mut id_sum: i32 = 0;
+    let mut power_sum: i32 = 0;
 
-    for line in input.lines(){
+    for line in input.lines() {
         let mut is_possible = true;
 
         let mut p1 = line.to_string();
@@ -29,7 +28,9 @@ fn part1(input: &str) -> i32 {
 
         let id = &p1.split_off(p1.find(' ').unwrap())[1..];
 
-        let sets = p2.split([',',';']).collect::<Vec<&str>>();
+        let sets = p2.split([',', ';']).collect::<Vec<&str>>();
+
+        let mut max_found = HashMap::from([("red", 0), ("green", 0), ("blue", 0)]);
 
         for set in sets {
             let parts = set[1..].split(' ').collect::<Vec<&str>>();
@@ -40,15 +41,19 @@ fn part1(input: &str) -> i32 {
             if count > available[color] {
                 is_possible = false;
             }
+
+            *max_found.get_mut(color).unwrap() =
+                std::cmp::max(*max_found.get(color).unwrap(), count);
         }
+
+        let power = max_found.values().fold(1, |acc, x| acc * x);
+
+        power_sum += power;
 
         if is_possible {
             id_sum += id.parse::<i32>().unwrap();
         }
     }
 
-    id_sum
+    (id_sum, power_sum)
 }
-
-// fn part2(input: &str) -> u32 {
-// }
