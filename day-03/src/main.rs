daf0b0ce8eed5353523b8320c07ded0070868e0c
azmonreal <azmonreal@gmail.com>
2023-12-03 10:41:17 -0600

    day 3: part 2

--- day-03/src/main.rs
+++ day-03/src/main.rs
@@ -8,6 +8,7 @@ fn main() {
     let contents = fs::read_to_string(file_path).expect("Something went wrong reading the file");
 
     println!("Part 1: {}", part1(&contents));
+    println!("Part 2: {}", part2(&contents));
 }
 
 fn part1(input: &str) -> i32 {
@@ -29,7 +30,6 @@ fn part1(input: &str) -> i32 {
 
     let cols = input.lines().nth(0).unwrap().chars().count() + 1;
 
-    println!("chars: {}, cols:{}", input.chars().count(), cols);
 
     for (i, line) in input.lines().enumerate() {
         let chars = line.chars();
@@ -41,7 +41,7 @@ fn part1(input: &str) -> i32 {
                     let x = i as i32 + cell.0;
                     let y = j as i32 + cell.1;
 
-                    if x < 0 || y < 0 || x >= input.lines().count() as i32 || y+1 >= cols as i32 {
+                    if x < 0 || y < 0 || x >= input.lines().count() as i32 || y + 1 >= cols as i32 {
                         continue;
                     }
 
@@ -70,4 +70,76 @@ fn part1(input: &str) -> i32 {
     }
 
     part_sum
+}
+
+fn part2(input: &str) -> u32 {
+    let mut part_num: u32 = 0;
+
+    let mut gear_sum: u32 = 0;
+
+    let cols = input.lines().nth(0).unwrap().chars().count() + 1;
+
+    let mut parts: Vec<(usize, usize, u32)> = Vec::new();
+    let mut gears: Vec<usize> = Vec::new();
+
+    let mut start: i32 = -1;
+
+    for (i, character) in input.chars().enumerate() {
+        if character.is_digit(10) {
+            if start == -1 {
+                start = i as i32;
+            }
+            part_num = part_num * 10 + character.to_digit(10).unwrap() as u32;
+        } else {
+            if start != -1 {
+                parts.push((start as usize, i, part_num));
+                start = -1;
+            }
+            part_num = 0;
+
+            if character == '*' {
+                gears.push(i);
+            }
+        }
+    }
+
+    let boundry = [
+        (-1, -1),
+        (-1, 0),
+        (-1, 1),
+        (0, -1),
+        (0, 1),
+        (1, -1),
+        (1, 0),
+        (1, 1),
+    ];
+
+    for gear in gears {
+        let x = (gear / cols) as i32;
+        let y = (gear % cols) as i32;
+
+        let mut p1 = -1;
+        let mut p2 = -1;
+
+        for cell in boundry {
+            let char_i = (x + cell.0) as usize * cols + (y + cell.1) as usize;
+
+            for (i, part) in parts.iter().enumerate() {
+                if (part.0..part.1).contains(&char_i) {
+                    if p1 == -1 {
+                        p1 = i as i32;
+                    } else if p1 != i as i32 {
+                        p2 = i as i32;
+                        break;
+                    }
+                }
+            }
+        }
+
+        if p1 != -1 && p2 != -1 {
+            gear_sum += parts[p1 as usize].2 * parts[p2 as usize].2;
+        }
+    }
+
+    gear_sum
 }
