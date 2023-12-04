b3828b1e181089591fc41a63c3b2070bcb650b5a
azmonreal <azmonreal@gmail.com>
2023-12-04 11:25:33 -0600

    day 4: part 2, combined

--- day-04/src/main.rs
+++ day-04/src/main.rs
@@ -7,11 +7,15 @@ fn main() {
 
     let contents = fs::read_to_string(file_path).expect("Something went wrong reading the file");
 
-    println!("Part 1: {}", part1(&contents));
+    let result = solve(&contents);
+
+    println!("Part 1: {}", result.0);
+    println!("Part 2: {}", result.1);
 }
 
-fn part1(input: &str) -> u32 {
-    let mut sum = 0;
+fn solve(input: &str) -> (u32, u32) {
+    let mut tickets_count: Vec<u32> = Vec::new();
+
     for line in input.lines() {
         let numbers = line.to_string().split_off(line.find(":").unwrap() + 2);
 
@@ -34,9 +38,29 @@ fn part1(input: &str) -> u32 {
             }
         }
 
+        tickets_count.push(count);
+    }
+
+    let point_sum: u32 = tickets_count.iter().fold(0, |acc, x| {
+        if x > &0 {
+            u32::pow(2, x - 1) + acc
+        } else {
+            acc
+        }
+    });
+
+    let mut copies: Vec<u32> = vec![1; tickets_count.len()];
+
+    for (i, count) in tickets_count.into_iter().enumerate() {
+        let copies_i = copies[i];
         if count > 0 {
-            sum += u32::pow(2, count - 1);
+            for copy in copies[i + 1..i + 1 + count as usize].iter_mut() {
+                *copy += copies_i;
+            }
         }
     }
-    sum
+
+    let copies_sum = copies.iter().sum::<u32>();
+
+    (point_sum, copies_sum)
 }
