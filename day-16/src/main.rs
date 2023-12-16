dfa516857dd09a71022dea16d5f77d53c36b4ece
azmonreal <azmonreal@gmail.com>
2023-12-16 13:13:44 -0600

    day 16: part 2, extracted to function

--- day-16/src/main.rs
+++ day-16/src/main.rs
@@ -7,6 +7,7 @@ fn main() {
     let contents = fs::read_to_string(file_path).expect("Something went wrong reading the file");
 
     println!("Part 1: {}", part1(&contents));
+    println!("Part 2: {}", part2(&contents));
 }
 
 #[derive(Debug, Hash, Eq, PartialEq, Copy, Clone)]
@@ -22,11 +23,37 @@ fn part1(contents: &String) -> usize {
         .lines()
         .map(|line| line.chars().collect::<Vec<char>>())
         .collect::<Vec<_>>();
+
+    count_energyzed(&mirrors, Direction::Right, (0, -1))
+}
+
+fn part2(contents: &String) -> usize {
+    let mirrors = contents
+        .lines()
+        .map(|line| line.chars().collect::<Vec<char>>())
+        .collect::<Vec<_>>();
+
+    let mut max = usize::MIN;
+
+    for r in 0..mirrors.len() {
+        max = max.max(count_energyzed(&mirrors, Direction::Right, (r as i32, -1)));
+        max = max.max(count_energyzed(&mirrors, Direction::Left, (r as i32, mirrors[r].len() as i32)));
+    }
+
+    for c in 0..mirrors[0].len() {
+        max = max.max(count_energyzed(&mirrors, Direction::Down, (-1, c as i32)));
+        max = max.max(count_energyzed(&mirrors, Direction::Up, (mirrors.len() as i32, c as i32)));
+    }
+
+    max
+}
+
+fn count_energyzed(mirrors: &Vec<Vec<char>>,dir: Direction, start: (i32, i32)) -> usize {
     let mut splits: Vec<(Direction, (i32, i32))> = Vec::new();
     let mut history: HashSet<(Direction, (i32, i32))> = HashSet::new();
     let mut energyzed: HashSet<(i32, i32)> = HashSet::new();
 
-    splits.push((Direction::Right, (0, -1)));
+    splits.push((dir, start));
 
     while let Some((dir, pos)) = splits.pop() {
         // println!(
@@ -56,7 +83,6 @@ fn part1(contents: &String) -> usize {
                 }
             }
             Direction::Down => {
-                next = match mirrors[pos.0 as usize + 1..]
                 next = match mirrors[(pos.0 + 1) as usize..]
                     .iter()
                     .map(|r| r[pos.1 as usize])
